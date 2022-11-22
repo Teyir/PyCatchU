b596e7bf829eb1c3615ac96a6727c88f3af7369f
HowBaka <gravy.thom@gmail.com>
2022-11-22 17:16:21 +0100

    [IMPROVE] Changed wording for menu

--- pages/home_page.py
+++ pages/home_page.py
@@ -40,7 +40,7 @@ class App(customtkinter.CTk):
         self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
 
         self.label_1 = customtkinter.CTkLabel(master=self.frame_left,
-                                              text="PyCatchU",
+                                              text="Menu",
                                               text_font=("Poppins", -16))  # font name and size in px
         self.label_1.grid(row=1, column=0, pady=10, padx=10)
 
