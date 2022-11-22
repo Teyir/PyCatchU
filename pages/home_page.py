a757edb068abb8a7b6b6c7cdaf1bb4279726efa7
HowBaka <gravy.thom@gmail.com>
2022-11-22 17:13:48 +0100

    [IMPROVE] Homepage V1

--- pages/home_page.py
+++ pages/home_page.py
@@ -2,12 +2,11 @@ import tkinter
 import tkinter.messagebox
 import customtkinter
 
-customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 
 class App(customtkinter.CTk):
-
     WIDTH = 780
     HEIGHT = 520
 
@@ -35,30 +34,25 @@ class App(customtkinter.CTk):
         # ============ frame_left ============
 
         # configure grid layout (1x11)
-        self.frame_left.grid_rowconfigure(0, minsize=10)   # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(0, minsize=10)  # empty row with minsize as spacing
         self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
-        self.frame_left.grid_rowconfigure(8, minsize=20)    # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(8, minsize=20)  # empty row with minsize as spacing
         self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
 
         self.label_1 = customtkinter.CTkLabel(master=self.frame_left,
                                               text="PyCatchU",
-                                              text_font=("Roboto Medium", -16))  # font name and size in px
+                                              text_font=("Poppins", -16))  # font name and size in px
         self.label_1.grid(row=1, column=0, pady=10, padx=10)
 
         self.button_1 = customtkinter.CTkButton(master=self.frame_left,
-                                                text="La daronne de Bodin",
+                                                text="All Pokemons",
                                                 command=self.button_event)
         self.button_1.grid(row=2, column=0, pady=10, padx=20)
 
         self.button_2 = customtkinter.CTkButton(master=self.frame_left,
-                                                text="Le cul de Cumrond",
+                                                text="Equipes",
                                                 command=self.button_event)
         self.button_2.grid(row=3, column=0, pady=10, padx=20)
-
-        self.button_3 = customtkinter.CTkButton(master=self.frame_left,
-                                                text="tamer",
-                                                command=self.button_event)
-        self.button_3.grid(row=4, column=0, pady=10, padx=20)
 
         self.label_mode = customtkinter.CTkLabel(master=self.frame_left, text="Theme:")
         self.label_mode.grid(row=9, column=0, pady=0, padx=20, sticky="w")
@@ -69,11 +63,21 @@ class App(customtkinter.CTk):
         self.optionmenu_1.grid(row=10, column=0, pady=10, padx=20, sticky="w")
 
         # ============ frame_right ============
+        img = tkinter.PhotoImage(file="./assets/logo.png")
+
+        self.button_3 = customtkinter.CTkButton(master=self.frame_right,
+                                      image=img, text="", fg_color=self.frame_right.fg_color, hover=False)
+        self.button_3.grid(row=2, column=1, pady=10, padx=20)
+
+        self.label_subtitle = customtkinter.CTkLabel(master=self.frame_right,
+                                                     text="Made with 🍆 by Thomas.T / Thomas.Gry / Axel.B / Valentin.B",
+                                                     text_font=("Poppins", -16))  # font name and size in px
+        self.label_subtitle.grid(row=3, column=1, pady=0, padx=0)
 
         # configure grid layout (3x7)
-        self.frame_right.rowconfigure((0, 1, 2, 3), weight=1)
-        self.frame_right.rowconfigure(7, weight=10)
-        self.frame_right.columnconfigure((0, 1), weight=1)
+        self.frame_right.rowconfigure((0, 1, 2, 3, 4, 5), weight=1)
+        self.frame_right.rowconfigure(6, weight=0)
+        self.frame_right.columnconfigure((0, 1, 3), weight=1)
         self.frame_right.columnconfigure(2, weight=0)
 
         # set default values
