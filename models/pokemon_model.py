3d7eebd0c94b7b7dfc28c9016b97e8adc9acfddc
ValentinBonneau <valentin.bonneau@epsi.fr>
2022-11-22 17:16:05 +0100

    Feat: Api Get all pokemon

--- models/pokemon_model.py
+++ models/pokemon_model.py
@@ -6,6 +6,8 @@ from PIL import Image
 
 class Pokemon:
     def __init__(self, id_or_name):
+        if type(id_or_name) == int:
+            id_or_name = str(id_or_name)
         poke = api.get_pokemon(id_or_name)
         species = api.get_pokemon_species(id_or_name)
         self.id = poke['id']
@@ -22,3 +24,7 @@ class Pokemon:
     def getHighQualitySprite(self):
         raw_data = api.get_image(self.spriteUrlHighQuality)
         return Image.open(io.BytesIO(raw_data))
+
+
+    def __str__(self):
+        return "#" + str(self.id) + " : " + self.name
