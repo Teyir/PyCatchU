64c15876fbcf8229482cf2f55889bbbbc9e919dc
Axebita1 <axel.baudoin@epsi.fr>
2022-11-23 11:05:11 +0100

    [Fix] Place to Grid + arrow change + Begining of Pokemon Page UI

--- pages/pokemon.py
+++ pages/pokemon.py
@@ -14,76 +14,85 @@ class PokemonPage(ctk.CTkFrame):
         self.pokemon_id = pokemon_id
         self.current_pokemon = pokemon_model.Pokemon(pokemon_id)
 
+        self.rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11), weight=1)
+        self.rowconfigure(12, weight=0)
+        self.columnconfigure((0, 1, 2, 3, 4), weight=1)
+        self.columnconfigure(5, weight=0)
+
     def setup(self):
         # Pokemon image
         img = download_file(self.current_pokemon.spriteUrlHighQuality).subsample(3, 3)
 
         self.master.pokemon_image = ui_kits.img(self, img)
-        self.master.pokemon_image.place(relx=0.25, rely=0.25)
+        self.master.pokemon_image.grid(row=0, column=2)
 
-        # Pokemon name
-        self.master.label_name = ctk.CTkLabel(master=self, text=self.current_pokemon.name)
-        self.master.label_name.place(relx=0.2, rely=0.3)
+        #  id
+        self.master.label_id = ctk.CTkLabel(master=self, text="Id : " + str(self.current_pokemon.id_formatted),
+                                            corner_radius=5
+                                            )
+        self.master.label_id.grid(row=1, column=2)
 
-        # Pokemon id
-        self.master.label_id = ctk.CTkLabel(master=self, text=self.current_pokemon.id_formatted)
-        self.master.label_id.place(relx=0.25, rely=0.3)
+        #  name
+        self.master.label_name = ctk.CTkLabel(master=self, text="Name : " + str(self.current_pokemon.name))
+        self.master.label_name.grid(row=2, column=2)
 
-        # Pokémon type
-        self.master.label_type = ctk.CTkLabel(master=self, text=self.current_pokemon.type)
-        self.master.label_type.place(relx=0.25, rely=0.3)
+        # type
+        self.master.label_type = ctk.CTkLabel(master=self, text="Type : " + str(self.current_pokemon.type))
+        self.master.label_type.grid(row=3, column=2)
 
         # STATS AREA
 
         # HP
-        self.master.label_hp = ctk.CTkLabel(master=self, text=self.current_pokemon.hp)
-        self.master.label_hp.place(relx=0.25, rely=0.3)
+        self.master.label_hp = ctk.CTkLabel(master=self, text="Heatlh :" + str(self.current_pokemon.hp))
+        self.master.label_hp.grid(row=4, column=2)
 
         # Attack
-        self.master.label_attack = ctk.CTkLabel(master=self, text=self.current_pokemon.attack)
-        self.master.label_attack.place(relx=0.25, rely=0.3)
+        self.master.label_attack = ctk.CTkLabel(master=self, text="Attack : " + str(self.current_pokemon.attack))
+        self.master.label_attack.grid(row=5, column=2)
 
         # Defense
-        self.master.label_defense = ctk.CTkLabel(master=self, text=self.current_pokemon.defense)
-        self.master.label_defense.place(relx=0.25, rely=0.3)
+        self.master.label_defense = ctk.CTkLabel(master=self, text="Defense : " + str(self.current_pokemon.defense))
+        self.master.label_defense.grid(row=6, column=2)
 
         # Sp Attack
-        self.master.label_sp_attack = ctk.CTkLabel(master=self, text=self.current_pokemon.sp_attack)
-        self.master.label_sp_attack.place(relx=0.25, rely=0.3)
+        self.master.label_sp_attack = ctk.CTkLabel(master=self,
+                                                   text="Special Attack : " + str(self.current_pokemon.sp_attack))
+        self.master.label_sp_attack.grid(row=7, column=2)
 
         # Sp Defense
-        self.master.label_sp_defense = ctk.CTkLabel(master=self, text=self.current_pokemon.sp_defense)
-        self.master.label_sp_defense.place(relx=0.25, rely=0.3)
+        self.master.label_sp_defense = ctk.CTkLabel(master=self,
+                                                    text="Special Defense : " + str(self.current_pokemon.sp_defense))
+        self.master.label_sp_defense.grid(row=8, column=2)
 
         # Speed
-        self.master.label_speed = ctk.CTkLabel(master=self, text=self.current_pokemon.speed)
-        self.master.label_speed.place(relx=0.25, rely=0.3)
+        self.master.label_speed = ctk.CTkLabel(master=self, text="Speed : " + str(self.current_pokemon.speed))
+        self.master.label_speed.grid(row=9, column=2)
 
         # Total Stats
-        self.master.label_total_stats = ctk.CTkLabel(master=self, text=self.current_pokemon.total_stats)
-        self.master.label_total_stats.place(relx=0.25, rely=0.3)
+        self.master.label_total_stats = ctk.CTkLabel(master=self,
+                                                     text="Puissance : " + str(self.current_pokemon.total_stats))
+        self.master.label_total_stats.grid(row=10, column=2)
 
         # Arrow change Pokémons
 
-        if self.pokemon_id > 1:
-            arrow_left = PhotoImage(file="./img/arrow_left.png").subsample(12, 12)
-            self.master.button_arrow_left = ctk.CTkButton(master=self, text="",
-                                                          image=arrow_left,
-                                                          fg_color=self.fg_color,
-                                                          command=self.pokemon_previous,
-                                                          hover=False)
-            self.master.button_arrow_left.place(relx=0.08, rely=0.75)
+        arrow_left = PhotoImage(file="./img/arrow_left.png").subsample(12, 12)
+        self.master.button_arrow_left = ctk.CTkButton(master=self, text="",
+                                                      image=arrow_left,
+                                                      fg_color=self.fg_color,
+                                                      command=self.pokemon_previous,
+                                                      hover=False)
+        self.master.button_arrow_left.grid(row=11, column=0)
 
-        if self.pokemon_id < 905:
-            arrow_right = PhotoImage(file="./img/arrow_right.png").subsample(12, 12)
-            self.master.button_arrow_right = ctk.CTkButton(master=self, text="",
-                                                           image=arrow_right,
-                                                           fg_color=self.fg_color,
-                                                           command=self.pokemon_next,
-                                                           hover=False)
-            self.master.button_arrow_right.place(relx=0.3, rely=0.75)
+        arrow_right = PhotoImage(file="./img/arrow_right.png").subsample(12, 12)
+        self.master.button_arrow_right = ctk.CTkButton(master=self, text="",
+                                                       image=arrow_right,
+                                                       fg_color=self.fg_color,
+                                                       command=self.pokemon_next,
+                                                       hover=False)
+        self.master.button_arrow_right.grid(row=11, column=4)
 
     def pokemon_previous(self):
+
         if self.pokemon_id <= 1:
             return
 
@@ -93,6 +102,7 @@ class PokemonPage(ctk.CTkFrame):
         self.master.frame_right.setup()
 
     def pokemon_next(self):
+
         if self.pokemon_id >= 905:
             return
 
