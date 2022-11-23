d083cdd4f09389d28fb2bf0c22ffa6b515a57ae8
Teyir <thomas.txr435@gmail.com>
2022-11-23 14:02:54 +0100

    [IMPROVE] Add dbManager

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from pages import main_page
+from manager import db_manager
+
+# Start sqlite
+conn = db_manager.DbManager().connection()
+if conn is not None:
+    db_manager.DbManager().init_db(conn)
+    # db_manager.DbManager().add_team(conn=conn, name="Test", pokemon_id_1=1, pokemon_id_2=2, pokemon_id_3=3, pokemon_id_4=4, pokemon_id_5=5)
+    print(db_manager.DbManager().fetch_teams(conn))
+    print(db_manager.DbManager().fetch_team(conn, team_id=1))
+else:
+    print("Error! cannot create the database connection.")
+
 
 app = main_page.App()
 app.mainloop()
---
+++ manager/db_manager.py
@@ -0,0 +1,71 @@
+import sqlite3
+from sqlite3 import Error
+
+
+class DbManager:
+    def __init__(self):
+        super().__init__()
+
+    def connection(self):
+        conn = None
+        try:
+            conn = sqlite3.connect("./data.db")
+            return conn
+        except Error as e:
+            print(e)
+
+        return conn
+
+    def init_db(self, conn):
+        sql_create_team = """CREATE TABLE IF NOT EXISTS teams (
+                              id INTEGER not null primary key autoincrement,
+                              name VARCHAR(255) NOT NULL ,
+                              pokemon_id_1 INT NOT NULL ,
+                              pokemon_id_2 INT NOT NULL ,
+                              pokemon_id_3 INT NOT NULL ,
+                              pokemon_id_4 INT NOT NULL ,
+                              pokemon_id_5 INT NOT NULL ,
+                              date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)"""
+
+        try:
+            c = conn.cursor()
+            c.execute('PRAGMA foreign_keys = ON')  # Add foreign key support
+            c.execute(sql_create_team)
+        except Error as e:
+            print(e)
+
+    def add_team(self, conn, name, pokemon_id_1, pokemon_id_2, pokemon_id_3, pokemon_id_4, pokemon_id_5):
+        cur = conn.cursor()
+        sql = """INSERT INTO teams (name, pokemon_id_1, pokemon_id_2, pokemon_id_3, pokemon_id_4, pokemon_id_5) 
+                    VALUES (?,?,?,?,?,?)"""
+        cur.execute(sql, (name, pokemon_id_1, pokemon_id_2, pokemon_id_3, pokemon_id_4, pokemon_id_5))
+        conn.commit()
+        return cur.lastrowid
+
+    def fetch_teams(self, conn):
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM teams")
+        return cur.fetchall()
+
+    def fetch_team(self, conn, team_id):
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM teams WHERE id = ?", (team_id,))
+        return cur.fetchall()[0]
+
+    def update_team(self, conn, team_id, name, pokemon_id_1, pokemon_id_2, pokemon_id_3, pokemon_id_4, pokemon_id_5):
+        cur = conn.cursor()
+        sql = """UPDATE teams SET name = ?,
+        pokemon_id_1 = ?,
+        pokemon_id_2 = ?,
+        pokemon_id_3 = ?,
+        pokemon_id_4 = ?,
+        pokemon_id_5 = ? WHERE id = ?"""
+        cur.execute(sql, (name, pokemon_id_1, pokemon_id_2, pokemon_id_3, pokemon_id_4, pokemon_id_5, team_id))
+        conn.commit()
+        return cur.lastrowid
+
+    def delete_team(self, conn, team_id):
+        sql = 'DELETE FROM teams WHERE id=?'
+        cur = conn.cursor()
+        cur.execute(sql, (id,))
+        conn.commit()
