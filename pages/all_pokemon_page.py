7fbfe70d3435457d9286cd293dd660a8620f8f98
Axebita1 <axel.baudoin@epsi.fr>
2022-11-24 18:58:09 +0100

    feat: searchbar

--- pages/all_pokemon_page.py
+++ pages/all_pokemon_page.py
@@ -1,8 +1,10 @@
 import customtkinter as ctk
+import re
 
 from models.pokemon_model import Pokemon
 from pages.home_page import HomePage
-from utils.requests_pokemon import get_all_pokemon
+from pages.pokemon import PokemonPage
+from utils.requests_pokemon import get_all_pokemon, getPixelSprite
 
 
 class AllPokemonPage(ctk.CTkFrame):
@@ -19,7 +21,6 @@ class AllPokemonPage(ctk.CTkFrame):
         self.columnconfigure(1, weight=10)
         self.rowconfigure((0, 2), weight=1)
         self.rowconfigure(1, weight=10)
-
 
         self.button_next = ctk.CTkButton(master=self, width=30, height=30, text=">", command=self.nextPage)
         self.button_next.grid(column=2, row=2)
@@ -50,30 +51,64 @@ class AllPokemonPage(ctk.CTkFrame):
         self.pokeList.columnconfigure((0, 1, 2, 3), weight=1)
         self.pokeList.rowconfigure((0, 1, 2), weight=1)
 
+        self.searchbar = ctk.CTkEntry(master=self, height=16, width=1000)
+        self.searchbar.grid(row=0, column=1)
+
+        self.buttonSearch = ctk.CTkButton(master=self, command=self.search, text='🔍')
+        self.buttonSearch.grid(row=0, column=2)
+
         self.pokeList.item = {}
 
         i = 0
 
         requests = get_all_pokemon(page=page, lenght=12)
 
-        for pokemon in requests:
-            o_pokemon = Pokemon(pokemon)
-            self.pokeList.item[pokemon] = PokeCard(o_pokemon, master=self.pokeList)
-            self.pokeList.item[pokemon].grid(column=i % 4, row=int(i / 4))
+        for pokemon in requests.keys():
+            self.pokeList.item[requests[pokemon]] = PokeCard(pokemon, requests[pokemon], master=self.pokeList)
+            self.pokeList.item[requests[pokemon]].grid(column=i % 4, row=int(i / 4))
             i += 1
+
+    def search(self):
+
+        self.pokeList.destroy()
+
+        self.pokeList = ctk.CTkFrame(master=self)
+        self.pokeList.grid(column=0, columnspan=3, row=1, sticky="nswe")
+        self.pokeList.columnconfigure((0, 1, 2, 3), weight=1)
+        self.pokeList.rowconfigure((0, 1, 2), weight=1)
+
+        string = self.searchbar.get()
+
+        if string != "":
+
+            self.pokeList.item = {}
+
+            pokemons = get_all_pokemon(lenght=905)
+
+            for poke in pokemons.keys():
+                i = len(self.pokeList.item)
+                if i >= 12:
+                    break
+                elif re.match(r"" + string, pokemons[poke]):
+                    self.pokeList.item[pokemons[poke]] = PokeCard(poke, pokemons[poke], master=self.pokeList)
+                    self.pokeList.item[pokemons[poke]].grid(column=i % 4, row=int(i / 4))
+
+        else:
+            self.drawPage()
 
 
 class PokeCard(ctk.CTkFrame):
-    def __init__(self, o_Pokemon, *args, **kwargs):
+    def __init__(self, id, name, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.pokemon = o_Pokemon
+        self.id = id
+        self.name = name
 
-        self.image = ctk.CTkButton(master=self, image=self.pokemon.getPixelSprite(), text="", fg_color=self.fg_color,
-                                   hover=False)
+        self.image = ctk.CTkButton(master=self, image=getPixelSprite(id), text="", fg_color=self.fg_color,
+                                   hover=False, command=self.clicked)
         self.image.grid(column=0)
 
-        self.title = ctk.CTkLabel(master=self, text=self.pokemon)
+        self.title = ctk.CTkLabel(master=self, text=self.name)
         self.title.grid(column=1)
 
         self.columnconfigure((0, 2), weight=1)
@@ -83,5 +118,5 @@ class PokeCard(ctk.CTkFrame):
 
     def clicked(self):
         self.master.master.master.frame_right.destroy()
-        self.master.master.master.frame_right = HomePage(master=self)
+        self.master.master.master.frame_right = PokemonPage(self.id, master=self.master.master.master)
         self.master.master.master.frame_right.setup()
--- utils/requests_pokemon.py
+++ utils/requests_pokemon.py
@@ -1,5 +1,6 @@
 import requests
 import re
+from PIL import Image,ImageTk
 
 def get_pokemons_amount():
     url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=1'
@@ -35,20 +36,27 @@ def get_pokemon_species(id_or_name):
 def get_image(url):
     return requests.get(url, stream=True).raw
 
-def get_all_pokemon(page = 1, lenght = None):
 
+def get_all_pokemon(page=1, lenght=None):
     if lenght is None:
         lenght = 50
 
     lenght = str(lenght)
-    if page < 1 :
+    if page < 1:
         url = "https://pokeapi.co/api/v2/pokemon/?limit=100000&offset=0"
     else:
-        page = str((page-1)*int(lenght))
-        url = "https://pokeapi.co/api/v2/pokemon/?limit="+lenght+"&offset=" + page
+        page = str((page - 1) * int(lenght))
+        url = "https://pokeapi.co/api/v2/pokemon/?limit=" + lenght + "&offset=" + page
     response = {}
     print(url)
     for poke in requests.get(url).json()["results"]:
-        response[int(re.search(r"(?<=pokemon/)[0-9]+",poke["url"]).group(0))] = poke["name"]
+        response[int(re.search(r"(?<=pokemon/)[0-9]+", poke["url"]).group(0))] = poke["name"]
 
     return response
+
+
+def getPixelSprite(id):
+    raw_data = get_image("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+str(id)+".png")
+    im = Image.open(raw_data)
+    return ImageTk.PhotoImage(im)
+
